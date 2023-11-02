3ece871205393635e4ffe960829bf77c15173da0
rishabhjain2000 <rishabhjainnitbhopal@gmail.com>
2023-11-03 01:02:53 +0530

    first commit

---
+++ backend/model/user.models.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema=new mongoose.model(
+    {
+        username:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true
+        },
+        password:{
+            type:String,
+            required:[true,"password is required"],
+        } 
+        
+    },
+    {
+        timestamps:true
+    }
+    )
+
+export const User=mongoose.model("User",userSchema); 
+//database me "Users" badal kar "users" ho jayega
